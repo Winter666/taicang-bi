afa348eed3f9863a9e54cd5290eb0ce5c1d43711
ZhaoZhenhua <1196464392@qq.com>
2016-03-05 21:33:51 +0800

    人口新增

--- tiger-web/src/main/java/tiger/web/controller/population/PopulationStructureController.java
+++ tiger-web/src/main/java/tiger/web/controller/population/PopulationStructureController.java
@@ -10,6 +10,7 @@ import tiger.common.data.dataobject.BirthrateDO;
 import tiger.common.data.dataobject.PopulationStructurePreDO;
 import tiger.core.base.BaseResult;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,6 +25,63 @@ public class PopulationStructureController {
 	        List<PopulationStructurePreDO> populationStructurePreDOList = populationStructurePreManager.getPopulationStructurePreData();
 
 	        System.out.println(populationStructurePreDOList.get(0).getSexType_id());
+	        List list=new ArrayList();
+	        
+	        List sum1=new ArrayList();
+	        double sum16=0;
+	        double sum17=0;
+	        double sum18=0;
+	        double sum19=0;
+	        double sum20=0;
+	        double sum21=0;
+	        double sum22=0;
+	        double sum23=0;
+	        double sum24=0;
+	        double sum25=0;
+	       
+	        for(int i=0;i<populationStructurePreDOList.size();i++){
+	        	if(populationStructurePreDOList.get(i).getPreYear()==2016){
+	        		 sum16+=populationStructurePreDOList.get(i).getPreResidentPopulation();
+	        	}
+	        	if(populationStructurePreDOList.get(i).getPreYear()==2017){
+	        		 sum17+=populationStructurePreDOList.get(i).getPreResidentPopulation();
+	        	}
+	        	if(populationStructurePreDOList.get(i).getPreYear()==2018){
+	        		 sum18+=populationStructurePreDOList.get(i).getPreResidentPopulation();
+	        	}
+	        	if(populationStructurePreDOList.get(i).getPreYear()==2019){
+	        		 sum19+=populationStructurePreDOList.get(i).getPreResidentPopulation();
+	        	}
+	        	if(populationStructurePreDOList.get(i).getPreYear()==2020){
+	        		 sum20+=populationStructurePreDOList.get(i).getPreResidentPopulation();
+	        	}
+	        	if(populationStructurePreDOList.get(i).getPreYear()==2021){
+	        		 sum21+=populationStructurePreDOList.get(i).getPreResidentPopulation();
+	        	}
+	        	if(populationStructurePreDOList.get(i).getPreYear()==2022){
+	        		 sum22+=populationStructurePreDOList.get(i).getPreResidentPopulation();
+	        	}
+	        	if(populationStructurePreDOList.get(i).getPreYear()==2023){
+	        		 sum23+=populationStructurePreDOList.get(i).getPreResidentPopulation();
+	        	}
+	        	if(populationStructurePreDOList.get(i).getPreYear()==2024){
+	        		 sum24+=populationStructurePreDOList.get(i).getPreResidentPopulation();
+	        	}
+	        	if(populationStructurePreDOList.get(i).getPreYear()==2025){
+	        		 sum25+=populationStructurePreDOList.get(i).getPreResidentPopulation();
+	        	}
+	        }
+	        sum1.add(sum16);
+	        sum1.add(sum17);
+	        sum1.add(sum18);
+	        sum1.add(sum19);
+	        sum1.add(sum20);
+	        sum1.add(sum21);
+	        sum1.add(sum22);
+	        sum1.add(sum23);
+	        sum1.add(sum24);
+	        sum1.add(sum25);
+	     
 
 	        return new BaseResult(populationStructurePreDOList);
 	    }
