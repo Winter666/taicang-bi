d46d49879d9cdecbb320f972ac77d04319ffee3f
Sam <1541491@tongji.edu.cn>
2016-03-08 08:54:24 +0800

    biz层account接口

---
+++ tiger-biz/src/main/java/tiger/biz/account/support/AccountManager.java
@@ -0,0 +1,41 @@
+/**
+ * 310 Inc.
+ * All Right Reserved.
+ */
+package tiger.biz.account.support;
+
+import tiger.core.domain.account.AccountDomain;
+import tiger.core.domain.account.AccountLoginLogDomain;
+
+/**
+ * Created by Bongo on 16/3/8.
+ */
+public interface AccountManager {
+
+    /**
+     * 用户登录接口
+     *
+     * @param account
+     * @param password
+     * @return
+     * */
+    AccountDomain signin(String account,String password);
+
+    /**
+     * 通过token获取账户信息
+     *
+     * @param token
+     * @return
+     * */
+    AccountDomain signinByToken(String token);
+
+    /**
+     * 为登录用户创建一个n天后过期的token
+     *
+     * @param accountLoginLogDomain
+     * @param expireDay
+     * @return
+     * */
+    String createToken(AccountLoginLogDomain accountLoginLogDomain,int expireDay);
+
+}
---
+++ tiger-biz/src/main/java/tiger/biz/account/support/impl/AccountManagerImpl.java
@@ -0,0 +1,16 @@
+/**
+ * 310 Inc.
+ * All Right Reserved.
+ */
+package tiger.biz.account.support.impl;
+
+import tiger.biz.account.support.AccountManager;
+import tiger.core.domain.account.AccountDomain;
+
+/**
+ * Created by Bongo on 16/3/8.
+ */
+public class AccountManagerImpl /*implements AccountManager*/{
+    
+
+}
