206bad527e037252beef9d9ab5e1b3662e24f0ad
Sam <1541491@tongji.edu.cn>
2016-03-09 02:13:39 +0800

    路径修改

--- tiger-web/src/main/java/tiger/web/controller/account/AccountController.java
+++ tiger-web/src/main/java/tiger/web/controller/account/AccountController.java
@@ -22,7 +22,7 @@ import javax.servlet.http.HttpServletResponse;
  * Created by Bongo on 16/3/9.
  */
 @RestController
-@RequestMapping("/account")
+@RequestMapping("/api/account")
 public class AccountController extends BaseController{
 
     @Autowired
